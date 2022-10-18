d12859f7a5026b824696ce4250e338f0782cf27b
Han-SeungJun <105290026+Han-SeungJun@users.noreply.github.com>
2022-10-19 00:20:03 +0900

    Create ml_listcrawl.py

---
+++ webcrawling_practice/beloved_church_address_mapping/inform_crawl/ml_listcrawl.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+# The function that appeded a list and crawled informations.
+def inform_list_crawler(_driver):
+    html = _driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    address_container = soup.find("div", "map_desc")
+
+    position = soup.find("h3").text
+    address = address_container.p.text.split("Tel. ")[0]
+    tel = address_container.p.text.split("Tel. ")[1]
+
+    each = {
+            "위치":position, "주소":address, "전화번호":tel
+    }
+    return each
